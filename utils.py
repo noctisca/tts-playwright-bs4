ac8ed1bd757ee3b23d6f36be465962c5ff51c84e
noctisca <205008316+noctisca@users.noreply.github.com>
2025-04-18 11:44:51 +0900

    fix

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 from urllib.parse import urlparse
 
-def save_to_file(content, filename="output/output.txt"):
+def save_to_file(content, filename="output/noname.json"):
     """ 取得したコンテンツをファイルに保存 """
     os.makedirs(os.path.dirname(filename), exist_ok=True)  # 保存先のディレクトリを作成
     with open(filename, 'w', encoding='utf-8') as f:
@@ -16,6 +16,6 @@ def generate_filename_from_url(url, output_dir="output"):
         filename_prefix = '-'.join(path_parts[:-1])
     else:
         # Use the full path as the filename if "-transcript" is not present
-        filename_prefix = '-'.join(path_parts) if path_parts else "output"
+        filename_prefix = '-'.join(path_parts) if path_parts else "noname"
 
-    return f"{output_dir}/{filename_prefix}.txt"
+    return f"{output_dir}/{filename_prefix}.json"
