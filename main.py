7c61ecd1ac7e7c7d01b3bf57ba6b69d59b23c266
noctisca <205008316+noctisca@users.noreply.github.com>
2025-04-19 16:42:17 +0900

    jsonファイル生成が済んでる場合は生成処理をスキップ

--- main.py
+++ main.py
@@ -9,30 +9,36 @@ from parser import HTMLParser
 from utils import save_to_file, generate_filename_from_url
 
 async def main(url):
-    translate_url = f"https://translate.google.com/translate?sl=auto&tl=ja&hl=ja&u={url}"
+    filename = generate_filename_from_url(url)
+    json_file_path = f"output/{filename}.json"  # JSONファイルのパスを生成
 
-    # スクレイピング処理
-    scraper = WebScraper(translate_url)
-    await scraper.fetch_content()
+    # JSONファイルがなければスクレイピング→保存
+    if not os.path.exists(json_file_path):
+        translate_url = f"https://translate.google.com/translate?sl=auto&tl=ja&hl=ja&u={url}"
 
-    # HTMLパース処理
-    parser = HTMLParser(scraper.get_content())
-    entry_content = parser.extract_conversation_structure()
+        # スクレイピング処理
+        scraper = WebScraper(translate_url)
+        await scraper.fetch_content()
 
-    # コンテンツが抽出できたら保存
-    if entry_content:
-        filename = generate_filename_from_url(url)
-        json_file_path = f"output/{filename}.json"  # JSONファイルのパスを生成
-        save_to_file(entry_content, json_file_path)
-        print(f"Content from {url} has been saved to {json_file_path}")
+        # HTMLパース処理
+        parser = HTMLParser(scraper.get_content())
+        entry_content = parser.extract_conversation_structure()
 
-        # 音声合成処理を呼び出し
-        synthesize_audio_from_json(json_file_path, filename)
-
-        # チャプター音声結合処理を呼び出し
-        concatenate_chapter_audio(json_file_path, filename)
+        # コンテンツが抽出できたら保存
+        if entry_content:
+            save_to_file(entry_content, json_file_path)
+            print(f"Content from {url} has been saved to {json_file_path}")
+        else:
+            print(f"Could not find content with class 'entry-content' on {url}.")
+            return
     else:
-        print(f"Could not find content with class 'entry-content' on {url}.")
+        print(f"既存のJSONファイルが見つかりました: {json_file_path}")
+
+    process_audio(json_file_path, filename)
+
+def process_audio(json_file_path, filename):
+    synthesize_audio_from_json(json_file_path, filename)
+    concatenate_chapter_audio(json_file_path, filename)
 
 def synthesize_audio_from_json(json_file, base_filename):
     with open(json_file, 'r', encoding='utf-8') as f:
